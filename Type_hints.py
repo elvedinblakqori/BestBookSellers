1e7c710b9126db19e7e8d690f4320cbe9755432d
Student <elvedinblakqori2022@gmail.com>
2025-04-26 03:40:59 -0700

    ...

--- Type_hints.py
+++ Type_hints.py
@@ -5,3 +5,9 @@ def get_fullname(first_name: str, last_name: str):
     return full_name
 
 print(get_fullname("john", "doe"))
+
+def get_items(item1 = str , item2 = int , item3 = float , item4 = bool ):
+    return get_items()
+
+print(get_items("John", 1, 1.5, True))
+
--- main.py
+++ main.py
@@ -9,3 +9,8 @@ def root():
 @app.get("/greet/")
 def read_root(name: str):
     return{"message": f"Hello, {name}!"}
+
+
+@app.get("/item/{item_id}")
+def read_items():
+    return
